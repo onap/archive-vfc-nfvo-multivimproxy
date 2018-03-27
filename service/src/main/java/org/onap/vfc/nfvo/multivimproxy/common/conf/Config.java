45ab470edd754384e6c8efc1c3f22b828f5c542e
victor.gao <victor.gao@huawei.com>
2018-03-27 14:01:37 +0800

    Add multivimproxy code
    Change-Id: I5189586830fb651e69a9984b0d42f78f160d95c4
    Issue-ID: VFC-644
    Signed-off-by: victor.gao <victor.gao@huawei.com>

---
+++ service/src/main/java/org/onap/vfc/nfvo/multivimproxy/common/conf/Config.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.vfc.nfvo.multivimproxy.common.conf;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.onap.vfc.nfvo.multivimproxy.common.constant.Constant;
+
+public class Config {
+
+    private static Properties prps = new Properties();
+
+    /**
+     * Private constructor.
+     */
+    private Config() {
+
+    }
+
+    static {
+        try {
+            prps.load(Config.class.getClassLoader().getResourceAsStream(Constant.CONF));
+        } catch(IOException e) {
+            throw new RuntimeException(e); // NOSONAR
+        }
+    }
+
+    public static String getHost() {
+        return prps.getProperty(Constant.HOST);
+    }
+
+    public static String getPort() {
+        return prps.getProperty(Constant.PORT);
+    }
+
+    public static String getCloudRegionId() {
+        return prps.getProperty(Constant.CLOUD_REGION_ID);
+    }
+
+    public static String getCloudOwner() {
+        return prps.getProperty(Constant.CLOUD_OWNER);
+    }
+
+    public static String getTenantId() {
+        return prps.getProperty(Constant.TENANT_ID);
+    }
+
+    public static String getGlobalCustomerId() {
+        return prps.getProperty(Constant.GLOBAL_CUSTOMER_ID);
+    }
+
+    public static String getServiceType() {
+        return prps.getProperty(Constant.SERVICE_TYPE);
+    }
+    
+    public static String getOpenstackPrefix() {
+    	return prps.getProperty("openstack_prefix");
+    }
+}
